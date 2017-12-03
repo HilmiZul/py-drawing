1f861517a6f3cf527222c05e782bbf1953009ddb
Zul Hilmi <netspytux@gmail.com>
2017-12-03 11:19:09 +0700

    Loop & Func

--- main.py
+++ main.py
@@ -9,14 +9,33 @@ form.title("Menggambar")
 kanvas = Canvas(form, width=lebar, height=tinggi, bg="#fff")
 kanvas.pack()
 
+def buat_lingkaran(koord, warna):
+  kanvas.create_oval(koord, fill=warna)
+
+def buat_persegi(koord, warna):
+  kanvas.create_rectangle(koord, fill=warna)
 
 # input koordinat
-x1 = input('X1: ')
-y1 = input('Y1: ')
-x2 = input('X2: ')
-y2 = input('Y2: ')
-warna = raw_input('Warna: ')
+gambar = True
+while gambar:
+  print "[1] Lingkaran"
+  print "[2] Persegi"
+  pilih = input("Pilih nomor: ")
 
-kanvas.create_rectangle((x1, y1, x2, y2), fill=warna)
+  if pilih == 1:
+    koord = x1, y1, x2, y2 = input('X1: '), input('Y1: '), input('X2: '), input('Y2: ')
+    warna = raw_input('Warna: ')
+    buat_lingkaran(koord, warna)
+  elif pilih == 2:
+    koord = x1, y1, x2, y2 = input('X1: '), input('Y1: '), input('X2: '), input('Y2: ')
+    warna = raw_input('Warna: ')
+    buat_persegi(koord, warna)
+
+  tanya = raw_input('Gambar lagi? [y/t] ')
+  if tanya == 'y':
+    gambar = True
+  else:
+    gambar = False
+    print "Close kanvas untuk mengakhiri program"
 
 form.mainloop()
